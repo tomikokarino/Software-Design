bdcc6594898df6e659f6bb59e81454a3cbfdde6c
Tomiko Karino <tomiko.student@gmail.com>
2022-12-04 22:17:31 -0500

    Dec 4 uploads

--- Interactive-web/main.js
+++ Interactive-web/main.js
@@ -1,36 +1,36 @@
+const slideShowModal = document.getElementById('slideShowModal')
 
-
-const exampleModal = document.getElementById('exampleModal')
-exampleModal.addEventListener('show.bs.modal', event => {
+slideShowModal.addEventListener('show.bs.modal', event => {
   // Image that triggered the modal
   const thumbnail = event.relatedTarget
   // Extract info from data-bs-* attributes
   const sliderType = thumbnail.getAttribute('data-bs-type')
-    console.log(sliderType)
-
+  
     let slideIndex = 1;
     showSlides(slideIndex);
     
     function plusSlides(n) {
-    showSlides(slideIndex += n);
-    }
-        
+      showSlides(slideIndex += n);
+    }    
+
     function showSlides(n) {
-        let i;
-        let slideType = document.getElementById(sliderType);
-        console.log(slideType);
-
-        let thisSlide = slideType.getElementsByClassName("slides");
-        console.log(thisSlide)
-
+      let i;
+      let slideType = document.getElementById(sliderType);
+      let thisSlide = slideType.getElementsByClassName("slides");
+      
+      if (slideType.id === sliderType) { 
+        slideType.style.display = "block";
+        console.log(thisSlide.length)
         if (n > thisSlide.length) {slideIndex = 1}    
         if (n < 1) {slideIndex = thisSlide.length}
         for (i = 0; i < thisSlide.length; i++) {
-            thisSlide[i].style.display = "none";  
+          thisSlide[i].style.display = "none";  
         }
-        
         thisSlide[slideIndex-1].style.display = "block";  
-        
-        }
-    })
+      }
+      else {
+        slideType.style.display = "none";
+      }
+     }
+})
    
